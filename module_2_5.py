f4bbc36d2e62bc690d9b869d891a732294e0e91c
Иван <ivan702007@gmail.com>
2024-12-12 13:09:39 +0300

    ДЗ_2_5

---
+++ module_2_5.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(0, n):
+        matrix.append([])
+        for j in range (0, m):
+            matrix[i].append(value)
+    return matrix
+
+
+print(get_matrix(4, 2 , 22))
